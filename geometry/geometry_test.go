675bcf38d959e22aaeebc45b7b5153c98efa846d
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2025-03-01 09:06:10 +0100

    learn about structs, methods and interfaces

---
+++ geometry/geometry_test.go
@@ -0,0 +1,41 @@
+package geometry
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	checkArea := func(t *testing.T, shape Shape, hasArea float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != hasArea {
+			t.Errorf("%#v got %.2f want %.2f", shape, got, hasArea)
+		}
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.hasArea)
+		})
+
+		checkArea(t, tt.shape, tt.hasArea)
+	}
+}
